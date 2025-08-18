6bd5c809c0f72e174b9a5848e3108cc9fb533b77
Nicholas Clark <nicholas.clark00@gmail.com>
2025-08-18 17:34:17 -0500

    add tcrdist_to_matrix function

--- R/utils.R
+++ R/utils.R
@@ -221,6 +221,15 @@ prep_for_tcrdist = function(df, params=NULL) {
   return(df)
 }
 
+tcrdist_to_matrix = function(tcr_obj) {
+  n_vert = dim(tcr_obj$tcr1)[1]
+  dist_df = tcr_obj$TCRdist_df %>% mutate %>%
+    mutate(edge1_1index = edge1_0index+1L,
+           edge2_1index = edge2_0index+1L
+           )
+  dist_df_to_matrix(dist_df, n_vert, 1:n_vert)
+}
+
 dist_df_to_matrix = function(dist_df, n_vertices_all, idx_keep) {
   ## temporarily switch zeros with -1
   dist_df$TCRdist_mod = ifelse(dist_df$TCRdist == 0L, -1L, dist_df$TCRdist)
